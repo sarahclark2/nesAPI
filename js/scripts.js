b9f4eb2cf67faaf5ccfc4d4ab2d57a5b91d12d9e
Sarah Clark <sarah8728@gmail.com>
2018-11-14 12:48:09 -0500

    first commit, set up the files

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  console.log('scripts loaded');
+
+var myKey = config.MY_KEY;
+
+var url = 'https://newsapi.org/v2/everything?q=bitcoin&from=2018-10-14&sortBy=publishedAt&apiKey=e4e20ca000ac4e90881678bff27c8325' + myKey;
+
+
+
+
+
+
+
+
+});
